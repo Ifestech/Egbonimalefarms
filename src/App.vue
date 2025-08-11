c06f8671765bfda17ea0c4edb3e6291d878d4fba
Egbonimale Raymond <egbonimaleraymornd2@gmail.com>
2025-08-10 21:56:09 -0700

    Added transition

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
 <template>
-  <div>
-     <Navbar />
-     <router-view />
+  <div class="opacity-0 transition-opacity duration-3000" :class="{ 'opacity-300': isLoaded }">
+    <Navbar />
+    <router-view />
   </div>
 </template>
 
 <script setup>
-import Navbar from './components/Navbar.vue';
+import Navbar from './components/Navbar.vue'; 
+import { ref, onMounted } from 'vue'
+
+const isLoaded = ref(false)
+
+onMounted(() => {
+  // Trigger fade-in when mounted
+  setTimeout(() => {
+    isLoaded.value = true
+  }, 50)
+})
 </script>
-
-<style scoped>
-
-</style>
