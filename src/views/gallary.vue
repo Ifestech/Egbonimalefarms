cd6f6819cec29e5048b1862ed1432c8d244b04a8
Egbonimale Raymond <egbonimaleraymornd2@gmail.com>
2025-08-11 23:02:09 -0700

    Update video autoplay and other changes

--- src/views/gallary.vue
+++ src/views/gallary.vue
@@ -9,11 +9,9 @@
       <source src="../assets/video/testimony1.mp4" type="video/mp4">
     </video>
 
-    <video controls muted loop class="w-[380px] h-[380px] object-cover rounded-2xl">
-      <source src="../assets/video/testimony2.mp4" type="video/mp4">
+    <video controls  muted loop class="w-[380px] h-[380px] object-cover rounded-2xl">
+      <source src="../assets/video/williams.mp4" type="video/mp4">
     </video>
-
-   
   </div>
    <div class="flex flex-col md:flex-row gap-10 bg-green-100 pt-10 px-3 mb-10 justify-center items-center">
   <img src="../assets/image/cassava1.png" alt="" class="w-[300px] h-[300px] object-contain">
